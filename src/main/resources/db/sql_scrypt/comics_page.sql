dbfea53abaf012c9c57864dc8c31d2d67dd38ff4
Ирина Моренова <i.a.morenova88@gmail.com>
2019-12-08 18:59:01 +0300

    comics_page.sql

--- src/main/resources/db/sql_scrypt/comics_page.sql
+++ src/main/resources/db/sql_scrypt/comics_page.sql
@@ -4,9 +4,9 @@
 
 CREATE TABLE public.comics_page
 (
-  comics_id int8 NOT NULL,
-  order_page int8,
-  path_file character varying(255),
+  comics_id bigint NOT NULL,
+  order_page bigint,
+  path_file character varying(255) NOT NULL,
   CONSTRAINT comics_page_pkey PRIMARY KEY (path_file),
   CONSTRAINT comics_page_comics_id_fkey FOREIGN KEY (comics_id)
       REFERENCES public.comics (id) MATCH SIMPLE
