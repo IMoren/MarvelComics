8c5c05429cfb3a9c1f518d12927df37fd8b1ae41
Ирина Моренова <i.a.morenova88@gmail.com>
2019-12-08 16:28:02 +0300

    указан формат дыты

--- src/main/java/ru/stm/marvelcomics/util/Const.java
+++ src/main/java/ru/stm/marvelcomics/util/Const.java
@@ -1,5 +1,7 @@
 package ru.stm.marvelcomics.util;
 
+import java.text.SimpleDateFormat;
+
 public class Const {
     public static final int LIM = 20;
     public static final int OFS = 0;
@@ -8,4 +10,6 @@ public class Const {
     public static final String PATH_FILE = "/home/data/comics_files";
     public static final String COMICS_DIR = "/comics";
     public static final String CHARACTER_DIR = "/character";
+    public static final String FORMAT_DATE_TO_STRING = "d-MM-yyyy";
+    public static final String FORMAT_STRING_TO_DATE = "d.MM.yyyy";
 }
