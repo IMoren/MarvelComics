c9e4887fa927ed1176d7be627a088d53ab185b83
Ирина Моренова <i.a.morenova88@gmail.com>
2019-12-08 18:35:27 +0300

    заданы константы для форматирования даты

--- src/main/java/ru/stm/marvelcomics/util/Const.java
+++ src/main/java/ru/stm/marvelcomics/util/Const.java
@@ -1,5 +1,6 @@
 package ru.stm.marvelcomics.util;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class Const {
@@ -7,9 +8,10 @@ public class Const {
     public static final int OFS = 0;
     public static final String LIMIT = "20";
     public static final String OFFSET = "0";
-    public static final String PATH_FILE = "/home/data/comics_files";
+    public static final String PATH_FILE = "/home/simsim/data/comics_files";
     public static final String COMICS_DIR = "/comics";
     public static final String CHARACTER_DIR = "/character";
-    public static final String FORMAT_DATE_TO_STRING = "d-MM-yyyy";
-    public static final String FORMAT_STRING_TO_DATE = "d.MM.yyyy";
+    public static final DateFormat FORMAT_DATE_TO_STRING = new SimpleDateFormat("d.MM.yyyy");
+    public static final DateFormat FORMAT_STRING_TO_DATE = new SimpleDateFormat("d-MM-yyyy");
+
 }
